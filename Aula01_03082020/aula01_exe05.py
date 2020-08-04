8002d8412dd70f03ac522953de05635e2663368d
mitchelloliveira <mitchelloliveira@hotmail.com>
2020-08-04 14:04:48 -0300

    Aula01.1
    Atividade 5 e 6

---
+++ Aula01_03082020/aula01_exe05.py
@@ -0,0 +1,4 @@
+val_m = float(input("Informe o valor em metros: "))
+val_cm = val_m * 1000
+print("O valor correspondente é: ", val_cm)
+print("O valor {}m corresponde a {}cm".format(val_m,val_cm))
---
+++ Aula01_03082020/aula01_exe06.py
@@ -0,0 +1,4 @@
+raio = int(input("Informe o valor do raio: "))
+area = 3.14159 * raio**2
+print("A área do circulo é: ", area)
+print("A área do circulo com raio {} é {} ".format(raio, area))
