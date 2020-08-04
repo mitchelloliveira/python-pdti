8b78c06797446abec78068f050fa5a533a884f75
mitchelloliveira <mitchelloliveira@hotmail.com>
2020-08-04 15:03:33 -0300

    Aula01.update4
    Atividades 9 e 10

---
+++ Aula01_03082020/aula01_exe09.py
@@ -0,0 +1,3 @@
+temperatura_f = float(input("Informe o valor da temperatura em Farenheit: "))
+temperatura_c = 5*((temperatura_f-32)/9)
+print("A temperatura em graus Celsius é: ", temperatura_c)
---
+++ Aula01_03082020/aula01_exe10.py
@@ -0,0 +1,3 @@
+temperatura_c = float(input("Informe o valor da temperatura em Celsius: "))
+temperatura_f = (temperatura_c * 9 / 5) + 32
+print("A temperatura em graus Farenheit é: ", temperatura_f)
