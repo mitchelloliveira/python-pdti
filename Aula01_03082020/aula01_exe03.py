7ef4ce31b7bdc19a6bfaa7fbdc3f7e6b03042907
mitchelloliveira <mitchelloliveira@hotmail.com>
2020-08-04 13:52:43 -0300

    Aula01
    Atividade 1 a 3

---
+++ Aula01_03082020/aula01_exe03.py
@@ -0,0 +1,5 @@
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo núemro: "))
+soma = num1 + num2
+print("A soma dos dois número informados é: ", soma)
+print("A soma de {} e {} é: {}".format(num1, num2, soma))
